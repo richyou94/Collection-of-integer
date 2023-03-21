aa1769410ad3494e003ee9dc57aa0d6b141c60d5
Richard You <richardyou94@gmail.com>
2023-03-20 22:36:32 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+    P - Print numbers
+    A - Add a number
+    M - Display mean of the numbers
+    S - Display the smallest number
+    L - Display the largest Number
+    Q - Quit
+    
+    Enter your choice
+
+     
+*/ 
