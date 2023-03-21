09780f4f8c0b9e9f88c41b90a324b1286cdef35d
Richard You <richardyou94@gmail.com>
2023-03-20 22:49:52 -0700

    default stage

--- main.cpp
+++ main.cpp
@@ -8,5 +8,44 @@
     
     Enter your choice
 
+ * if illegal choice is made, display "Unknown selection, please try again"'
+ * if users enter P or p, should display all of the elements (ints) in the list.
+ * if list is empty you should display "[] - the list is empty"
+ * if is not empty, display inside [] separated by sapce
+ * 
+ * enter a , prompt the user for an integer to add to the list
+ * if user enters 5, "5 added"
+ * 
+ * if user enters m, calculate mean of elements in the list
+ * if empty, "Unable to calculate the mean - no data"
+ * 
+ * if user enters s display smallest element in the list
+ * "The smallest number is ?"
+ * if empty, "unable to determine the largest number - list is empty"
+ * 
+ * if enters l, display largest element in the list,
+ * "The largest number is ?"
+ * if list is empty, "Unable to determine the largest number - list is empty"
+ * 
+ * if user enters q, then "Goodbye" program terminates
+ * 
+ * Additional functionality, 
+ * - search for number in the list and if found display the number of times it occurs in the list
+ * clearing out the list, (make it empty again) , vector class has .clear() method
+ * don't allow duplicate entries
+ * come up with my own ideas. 
+ * 
      
 */ 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    
+    
+    cout << endl;
+    return 0;
+}
